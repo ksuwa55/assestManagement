547cb107ff6aee0287179fd1a3864b61633e166b
kazuki <klklklkl063055@gmail.com>
2023-06-11 15:11:08 +0900

    add backend

---
+++ backend/src/test/java/assetmanagement/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package assetmanagement.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
