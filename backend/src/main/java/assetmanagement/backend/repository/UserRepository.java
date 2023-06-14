54c963241d3c2d490b6067fd6908e4ca486f19d0
kazuki <klklklkl063055@gmail.com>
2023-06-15 00:15:57 +0900

    create user table

--- backend/src/main/java/assetmanagement/backend/repository/UserRepository.java
+++ backend/src/main/java/assetmanagement/backend/repository/UserRepository.java
@@ -2,9 +2,11 @@ package assetmanagement.backend.repository;
 
 import assetmanagement.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
