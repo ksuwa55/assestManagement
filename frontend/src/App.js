a6a3e1e90210f1faf1f7c5c81eaf75b23465c6ca
kazuki <klklklkl063055@gmail.com>
2023-07-15 19:35:54 +0900

    add input field

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignupForm from './pages/signup';
 import LoginForm from './pages/login';
 import Home from './pages/home';
@@ -8,26 +8,32 @@ import Portfolio from './pages/portfolio';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  
-  const isLoggedIn = localStorage.getItem('authenticated'); // Replace with the auth logic
+const App = () => {
+  const isLoggedIn = localStorage.getItem('authenticated');
 
   return (
     <div className="App">
       <Router>
-          <Routes>
-            <Route path='/signup' Component={SignupForm}/>
-            <Route path='/login' Component={LoginForm}/>
-            <Route path='/search' Component={Search}/>
-            <Route path='/portfolio' Component={Portfolio}/>
+        <Routes>
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/portfolio" element={<Portfolio />} />
 
-            <Route path='/' 
-            element={isLoggedIn ? <Home />: <Navigate to={"/login"} replace/>}
-            />
-          </Routes>
+          <Route
+            path="/"
+            element={
+              isLoggedIn ? (
+                <Home />
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
+        </Routes>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/pages/search.js
+++ frontend/src/pages/search.js
@@ -8,10 +8,53 @@ const Search = () => {
       <Navbar />
       <div className="flex-grow-1 d-flex">
         <Sidebar />
-        <div className="flex-grow-1 d-flex align-items-center justify-content-center" style={{ flex: '1' }}>
-          <div className="text-center">
-            <h1>Welcome to the Search page</h1>
+        <div className='container'>
+        <div class="input-group input-group-sm mb-3">
+          <div className=' m-5 '>
+            <input type="text" class="form-control border-dark mb-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+            <button type="button" class="btn btn-primary">Primary</button>
           </div>
+          <div className=' m-5 '>
+            <input type="text" class="form-control border-dark mb-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+            <button type="button" class="btn btn-primary">Primary</button>
+          </div>
+          <div className=' m-5 '>
+            <input type="text" class="form-control border-dark mb-3" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+            <button type="button" class="btn btn-primary">Primary</button>
+          </div>        
+        </div>
+        <div className="flex-grow-1 d-flex align-items-center justify-content-center p-5" style={{ flex: '1' }}>
+          <table class="table table-bordered border-dark">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         </div>
       </div>
     </div>
