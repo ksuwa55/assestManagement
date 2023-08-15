ac7f281841ba1e75a00f993651b6d3e2ff6826bb
kazuki <klklklkl063055@gmail.com>
2023-08-15 20:38:17 +0900

    display result from the web page to my local page

--- backend/src/main/java/assetmanagement/backend/service/StockService.java
+++ backend/src/main/java/assetmanagement/backend/service/StockService.java
@@ -1,61 +1,46 @@
-package assetmanagement.backend.service;
+    package assetmanagement.backend.service;
 
-import assetmanagement.backend.model.Stock;
-import assetmanagement.backend.response.AlphaVantageResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+    import assetmanagement.backend.model.Stock;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+    import java.io.BufferedReader;
+    import java.io.FileReader;
+    import java.io.IOException;
+    import java.util.ArrayList;
+    import java.util.List;
 
-@Service
-public class StockService {
-    @Autowired
-    private WebScraperService webScraperService;
-    private static final String DOW_SYMBOLS_FILE_PATH = "C:\\Users\\ksuwa\\Documents\\Coding\\assetManagement\\backend\\src\\main\\resources\\static\\dow_symbols.txt";
+    @Service
+    public class StockService {
+        @Autowired
+        private WebScraperService webScraperService;
+        private static final String DOW_SYMBOLS_FILE_PATH = "C:\\Users\\ksuwa\\Documents\\Coding\\assetManagement\\backend\\src\\main\\resources\\static\\dow_symbols.txt";
 
-    public List<Stock> searchStocksByCriteria(List<String> stockSymbols, double price, double market_capita){
-        List<Stock> filteredStocks = new ArrayList<>();
+        public List<Stock> searchStocksByCriteria(List<String> stockSymbols, double price, double market_capita) {
+            List<Stock> filteredStocks = new ArrayList<>();
 
-        for (String symbol : stockSymbols) {
+            List<Stock> stockInfoList = webScraperService.scrapeStockInfoList();
 
-            // Process the API response to get the real-time stock data
-            Stock stockInfo = (Stock) webScraperService.scrapeStockInfoList();
-
-            // Filter stocks based on criteria and add to the result list
-            if (stockInfo != null && stockInfo.getPrice() <= price && stockInfo.getMarket_capita() <= market_capita) {
-                filteredStocks.add(stockInfo);
+            for (Stock stockInfo : stockInfoList) {
+                if (stockSymbols.contains(stockInfo.getSymbol()) &&
+                        stockInfo.getPrice() <= price && stockInfo.getMarket_capita() <= market_capita) {
+                    filteredStocks.add(stockInfo);
+                }
             }
 
+            return filteredStocks;
         }
-        return filteredStocks;
-    }
 
-    public List<String> getDowSymbolsFromFile() {
-        List<String> stockSymbols = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(DOW_SYMBOLS_FILE_PATH))){
-            String line;
-            while ((line = br.readLine()) != null){
-                stockSymbols.add(line.trim());
+        public List<String> getDowSymbolsFromFile() {
+            List<String> stockSymbols = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(DOW_SYMBOLS_FILE_PATH))){
+                String line;
+                while ((line = br.readLine()) != null){
+                    stockSymbols.add(line.trim());
+                }
+            } catch (IOException e){
+                e.printStackTrace();
             }
-        } catch (IOException e){
-            e.printStackTrace();
+            return stockSymbols;
         }
-        return stockSymbols;
     }
-    private Stock extractStockInfoFromApiResponse(AlphaVantageResponse response) {
-        // Extract relevant data from the Alpha Vantage API response and create a single StockInfo object
-        Stock stockInfo = new Stock();
-        stockInfo.setSymbol(response.getSymbol());
-        stockInfo.setName(response.getName());
-//        stockInfo.setPer(response.getPriceToEarningRatio());
-//        stockInfo.setPbr(response.getPriceToBookRatio());
-//        stockInfo.setRoe(response.getReturnOnEquity());
-        // Add other fields as needed based on the actual response structure
-        return stockInfo;
-    }
-}
--- backend/src/main/java/assetmanagement/backend/service/WebScraperService.java
+++ backend/src/main/java/assetmanagement/backend/service/WebScraperService.java
@@ -5,11 +5,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class WebScraperService {
 
     public List<Stock> scrapeStockInfoList() {
