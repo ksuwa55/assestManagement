6d0f59f957461d77f1323df677c21d70ad71d1af
kazuki <klklklkl063055@gmail.com>
2023-12-08 09:42:03 +0900

    delete unnecessary import

--- backend/src/main/java/assetmanagement/backend/repository/PortfolioRepository.java
+++ backend/src/main/java/assetmanagement/backend/repository/PortfolioRepository.java
@@ -1,11 +1,8 @@
 package assetmanagement.backend.repository;
 
 import assetmanagement.backend.model.Portfolio;
-import assetmanagement.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
