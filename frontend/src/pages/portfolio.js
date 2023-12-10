7520a49bd946ec886eb8f0436f54c919d1732366
kazuki <klklklkl063055@gmail.com>
2023-12-10 15:15:12 +0900

    add table to portfolio

--- frontend/src/pages/portfolio.js
+++ frontend/src/pages/portfolio.js
@@ -8,10 +8,27 @@ const Portfolio = () => {
       <Navbar />
       <div className="flex-grow-1 d-flex">
         <Sidebar />
-        <div className="flex-grow-1 d-flex align-items-center justify-content-center" style={{ flex: '1' }}>
-          <div className="text-center">
-            <h1>Welcome to the Poftfolio page</h1>
-          </div>
+        <div className="flex-grow-1 d-flex align-items-center justify-content-center p-5" style={{ flex: '1' }}>
+          <table className="table table-striped table-bordered border-dark">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Market Capitalization</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+                <tr>
+                  <td>AAAA</td>
+                  <td>AAAA</td>
+                  <td>AAAA</td>
+                  <td>
+                    <button type="button" className="btn btn-danger">×</button>
+                  </td>
+                </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
