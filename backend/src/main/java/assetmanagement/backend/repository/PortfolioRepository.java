1e35fbe0dac442de0cacc99dc4f514dc2d33f31f
kazuki <klklklkl063055@gmail.com>
2023-12-12 20:00:48 +0900

    add a query in PortfolioRepository

--- backend/src/main/java/assetmanagement/backend/repository/PortfolioRepository.java
+++ backend/src/main/java/assetmanagement/backend/repository/PortfolioRepository.java
@@ -2,8 +2,15 @@ package assetmanagement.backend.repository;
 
 import assetmanagement.backend.model.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+    @Query("SELECT p.stockSymbol FROM Portfolio p WHERE p.userId = :userId")
+    List<String> findStockSymbolsByUserId(@Param("userId") Long userId);
+
 }
